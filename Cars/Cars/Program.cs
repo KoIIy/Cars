35e0722bc992367f353d8a2c80da9e8243150e08
KoIIy <boss.sergo.2017@mail.ru>
2022-03-29 01:20:23 +0300

    create app and data dictionary and add Users

---
+++ Cars/Cars/Program.cs
@@ -0,0 +1,25 @@
+﻿using Cars.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CarsEntities db = new CarsEntities())
+            {
+                var cars = db.Car;
+                foreach (Car t in cars)
+                    Console.WriteLine("\t{0}\t{1}{2}\t{3}\t{4}", t.CarID,t.Number,t.Region,t.InsuranceNumber,t.Color) ;
+                Console.WriteLine("-----------------------------------------------------------------");
+            
+            }
+            Console.ReadKey();
+        }
+    }
+}
